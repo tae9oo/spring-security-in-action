1b33623d449d7eca1b73d2ac8da4343fb7850bd0
taegoo <taegookim0814@gmail.com>
2023-08-13 02:58:50 +0900

    [main] init project

---
+++ src/test/java/com/taegoo/springsecurityinaction/SpringSecurityInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taegoo.springsecurityinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
